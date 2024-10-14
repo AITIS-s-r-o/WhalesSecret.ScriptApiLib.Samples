8b148ea9bfc1924e7ab1b127eca3a0b1751e812f
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-10-14 13:26:25 +0200

    WIP

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using WhalesSecret.ScriptApiLib;
+using WhalesSecret.ScriptApiLib.Exchanges;
+using WhalesSecret.TradeScriptLib.API.TradingV1;
+using WhalesSecret.TradeScriptLib.API.TradingV1.MarketData;
+using WhalesSecret.TradeScriptLib.Entities;
+using WhalesSecret.TradeScriptLib.Entities.MarketData;
+
+namespace WhalesSecret.ScriptApiLibConsoleApp;
+
+public class Program
+{
+    /// <summary>
+    /// Application that fetches new ticker data and displays it.
+    /// </summary>
+    /// <param name="args">Command-line arguments.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    private static async Task Main(string[] args)
+    {
+        ScriptApi api = await ScriptApi.CreateAsync(appDataFolder: "data", connectToBinanceSandbox: false, connectToKucoinSandbox: false);
+
+        string? identityFolder = Environment.GetEnvironmentVariable("WhalesSecretPrivateAssets");
+
+        if (identityFolder is null)
+            throw new InvalidOperationException("Please set 'WhalesSecretPrivateAssets' environment variable.");
+
+        string credentialsJson = File.ReadAllText(Path.Combine(identityFolder,  "BinanceApiIdentity.json"));
+        using BinanceApiIdentity apiIdentity = BinanceApiIdentity.CreateFromJson(credentialsJson);
+
+        api.SetCredentials(apiIdentity);
+
+        ITradeApiClient client = await api.TradeApiV1.ConnectAsync(ExchangeMarket.BinanceSpot).ConfigureAwait(false);
+        ITickerSubscription tickerSubscription = await client.CreateTickerSubscriptionAsync(SymbolPair.BTC_USDT, CancellationToken.None).ConfigureAwait(false);
+
+        while (true)
+        {
+            Ticker ticker = await tickerSubscription.GetNewerTickerAsync().ConfigureAwait(false);
+            Console.WriteLine($"Ticker is: {ticker}");
+            Debug.WriteLine($"Ticker is: {ticker}");
+        }
+    }
+}
