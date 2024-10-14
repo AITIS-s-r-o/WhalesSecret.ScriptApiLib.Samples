73fb2a184425e0a97c76964d37ba62c0e8d42d56
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-10-14 16:35:24 +0200

    Remove identity

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using WhalesSecret.ScriptApiLib;
-using WhalesSecret.ScriptApiLib.Exchanges;
 using WhalesSecret.TradeScriptLib.API.TradingV1;
 using WhalesSecret.TradeScriptLib.API.TradingV1.MarketData;
 using WhalesSecret.TradeScriptLib.Entities;
@@ -17,19 +16,9 @@ public class Program
     /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     private static async Task Main(string[] args)
     {
-        ScriptApi api = await ScriptApi.CreateAsync(appDataFolder: "data", connectToBinanceSandbox: false, connectToKucoinSandbox: false);
-
-        string? identityFolder = Environment.GetEnvironmentVariable("WhalesSecretPrivateAssets");
-
-        if (identityFolder is null)
-            throw new InvalidOperationException("Please set 'WhalesSecretPrivateAssets' environment variable.");
-
-        string credentialsJson = File.ReadAllText(Path.Combine(identityFolder, "BinanceApiIdentity.json"));
-        using BinanceApiIdentity apiIdentity = BinanceApiIdentity.CreateFromJson(credentialsJson);
-
-        api.SetCredentials(apiIdentity);
-
-        await using ITradeApiClient client = await api.TradeApiV1.ConnectAsync(ExchangeMarket.BinanceSpot).ConfigureAwait(false);
+        await using ScriptApi api = await ScriptApi.CreateAsync(appDataFolder: "data", connectToBinanceSandbox: false, connectToKucoinSandbox: false);
+        ConnectionOptions connectionOptions = new(connectionType: ConnectionType.MarketData);
+        await using ITradeApiClient client = await api.TradeApiV1.ConnectAsync(ExchangeMarket.BinanceSpot, connectionOptions).ConfigureAwait(false);
         await using ITickerSubscription tickerSubscription = await client.CreateTickerSubscriptionAsync(SymbolPair.BTC_USDT, CancellationToken.None).ConfigureAwait(false);
 
         while (true)
