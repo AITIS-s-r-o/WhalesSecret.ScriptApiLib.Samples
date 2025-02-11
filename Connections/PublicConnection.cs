bd64ada91be0ad657b703a4ace934cc841c22247
dejfcz <dejfcz@users.noreply.github.com>
2025-02-11 15:39:55 +0000

    connection samples

---
+++ Connections/PublicConnection.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WhalesSecret.TradeScriptLib.API.TradingV1;
+using WhalesSecret.TradeScriptLib.Entities;
+
+namespace WhalesSecret.ScriptApiLib.Samples.Connections;
+
+/// <summary>
+/// Sample that demonstrates how to connect to an exchange market using a public connection. Public connections are commonly used for accessing publicly available information,
+/// such as candlesticks, order books, or tickers.
+/// </summary>
+public class PublicConnection : IScriptApiSample
+{
+    /// <inheritdoc/>
+    public async Task RunSampleAsync(ExchangeMarket exchangeMarket)
+    {
+        using CancellationTokenSource timeoutCts = new(TimeSpan.FromMinutes(2));
+
+        await using ScriptApi scriptApi = await ScriptApi.CreateAsync().ConfigureAwait(false);
+
+        // Initialization of the market is required before connection can be created.
+        await Console.Out.WriteLineAsync($"Initialize exchange market {exchangeMarket}.").ConfigureAwait(false);
+        _ = await scriptApi.InitializeMarketAsync(exchangeMarket, timeoutCts.Token).ConfigureAwait(false);
+
+        await Console.Out.WriteLineAsync($"Connect to {exchangeMarket} exchange with a public connection.").ConfigureAwait(false);
+
+        // Market-data connection type is the only connection type that does not need exchange API credentials.
+        ConnectionOptions connectionOptions = new(connectionType: ConnectionType.MarketData);
+        await using ITradeApiClient tradeClient = await scriptApi.ConnectAsync(exchangeMarket, connectionOptions).ConfigureAwait(false);
+
+        await Console.Out.WriteLineAsync($"Public connection to {exchangeMarket} has been established successfully.").ConfigureAwait(false);
+
+        // As the connection is established, we can use the connected client to, for example, query the time of the exchange.
+        DateTime utcExchangeTime = tradeClient.GetExchangeUtcDateTime();
+        TimeSpan diff = DateTime.UtcNow - utcExchangeTime;
+
+        await Console.Out.WriteLineAsync($"Current UTC time of the {exchangeMarket} exchange is {utcExchangeTime}. The difference between the local time and the exchange time is {
+            diff}.").ConfigureAwait(false);
+
+        await Console.Out.WriteLineAsync("Disposing trade API client and script API.").ConfigureAwait(false);
+    }
+}
---
+++ Credentials.cs
@@ -0,0 +1,53 @@
+using WhalesSecret.ScriptApiLib.Exchanges;
+using WhalesSecret.TradeScriptLib.Entities;
+
+namespace WhalesSecret.ScriptApiLib.Samples;
+
+/// <summary>
+/// Container of exchange API credentials for samples that need to establish private connection.
+/// </summary>
+/// <remarks>In order for those samples to work, you have to set valid credentials below. You only need to change credentials that you are going to use.</remarks>
+public static class Credentials
+{
+    /// <summary>API key for Binance exchange using HMAC algorithm.</summary>
+    public const string BinanceHmacApiKey = "CHANGE THIS Binance HMAC API key";
+
+    /// <summary>API key for Binance exchange using RSA algorithm.</summary>
+    public const string BinanceRsaApiKey = "CHANGE THIS Binance RSA API key";
+
+    /// <summary>API key for Kucoin exchange.</summary>
+    public const string KucoinApiKey = "CHANGE THIS Kucoin API key";
+
+    /// <summary>API secret for Binance exchange using HMAC algorithm.</summary>
+    public static readonly SensitiveByteArray BinanceHmacSecret = "CHANGE THIS Binance HMAC API secret"u8;
+
+    /// <summary>API secret for Binance exchange using RSA algorithm.</summary>
+    public static readonly SensitiveByteArray BinanceRsaSecret = "CHANGE THIS Binance RSA API secret"u8;
+
+    /// <summary>API secret for Kucoin exchange.</summary>
+    public static readonly SensitiveByteArray KucoinSecret = "CHANGE THIS Kucoin secret"u8;
+
+    /// <summary>API passphrase for Kucoin exchange.</summary>
+    public static readonly SensitiveByteArray KucoinPassphrase = "CHANGE THIS Kucoin passphrase"u8;
+
+    /// <summary>
+    /// Gets exchange API credentials for Binance exchange using HMAC algorithm.
+    /// </summary>
+    /// <returns>Exchange API credentials for Binance exchange using HMAC algorithm.</returns>
+    public static IApiIdentity GetBinanceHmacApiIdentity()
+        => BinanceApiIdentity.CreateHmac(name: "BinanceHmacCredentials", key: BinanceHmacApiKey, BinanceHmacSecret);
+
+    /// <summary>
+    /// Gets exchange API credentials for Binance exchange using RSA algorithm.
+    /// </summary>
+    /// <returns>Exchange API credentials for Binance exchange using RSA algorithm.</returns>
+    public static IApiIdentity GetBinanceRsaApiIdentity()
+        => BinanceApiIdentity.CreateRsa(name: "BinanceRsaCredentials", key: BinanceRsaApiKey, BinanceRsaSecret);
+
+    /// <summary>
+    /// Gets exchange API credentials for Kucoin exchange.
+    /// </summary>
+    /// <returns>Exchange API credentials for Kucoin exchange.</returns>
+    public static IApiIdentity GetKucoinApiIdentity()
+        => KucoinApiIdentity.Create(name: "KucoinCredentials", key: KucoinApiKey, secret: KucoinSecret, passphrase: KucoinPassphrase);
+}
