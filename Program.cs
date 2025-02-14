db186f495c030344e5d90a336520ee2b35f4855d
dejfcz <dejfcz@users.noreply.github.com>
2025-02-14 13:52:06 +0000

    rev

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ public class Program
     {
         new object[] { "Accounts/ExchangeAccount", typeof(ExchangeAccount), "Demonstrates how to get balances for assets in the exchange account wallet and basic trading fees."},
         new object[] { "Connections/Public", typeof(PublicConnection), "Demonstrates how to connect to an exchange via public connection."},
-        new object[] { "Connections/Private", typeof(SizeSmall), "Demonstrates how to connect to an exchange via private connection using exchange API credentials."},
+        new object[] { "Connections/Private", typeof(PrivateConnection), "Demonstrates how to connect to an exchange via private connection using exchange API credentials."},
         new object[] { "Exchanges/ExchangeInformation", typeof(ExchangeInformation), "Demonstrates what kind of information can we get from the exchange market initialization."},
         new object[] { "Orders/Size.Small", typeof(SizeSmall), "Basic order sample. Demonstrates how small sized orders can be placed."},
         new object[] { "Orders/Size.Large", typeof(SizeLarge), "Basic order sample. Demonstrates how larger sized orders can be placed. Requires valid license."},
