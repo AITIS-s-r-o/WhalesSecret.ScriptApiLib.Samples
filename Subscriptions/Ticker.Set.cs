9d18a98c85dbda4ae068c8dcde6ac12094da37fc
dejfcz <dejfcz@users.noreply.github.com>
2025-02-19 08:05:48 +0000

    Update Subscriptions/Ticker.Set.cs
    Co-authored-by: MartyIX <203266+MartyIX@users.noreply.github.com>

--- Subscriptions/Ticker.Set.cs
+++ Subscriptions/Ticker.Set.cs
@@ -51,7 +51,7 @@ public class TickerSet : IScriptApiSample
             for (int i = 0; i < 10; i++)
             {
                 // Note that we are not guaranteed to get any number of updates for any particular symbol pair. Also note that when a subscription is created, we get an initial
-                // state, which is propagated as an update, but it may be preceeded with any number of updates of earlier subscribed symbol pairs of the same set.
+                // state, which is propagated as an update, but it may be preceded with any number of updates of earlier subscribed symbol pairs of the same set.
                 Ticker ticker = await subscriptionSet.WhenAnyNewTickerAsync().ConfigureAwait(false);
                 await Console.Out.WriteLineAsync($"  {DateTime.UtcNow} | New ticker update received: {ticker}").ConfigureAwait(false);
             }
