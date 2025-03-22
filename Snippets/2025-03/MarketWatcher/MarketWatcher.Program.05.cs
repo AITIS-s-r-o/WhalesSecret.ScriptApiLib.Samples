f1997c7c4b8477e87a9247e11f9889d7e192ddee
MartyIX <203266+MartyIX@users.noreply.github.com>
2025-03-22 12:18:33 +0100

    update

--- Snippets/2025-03/MarketWatcher/MarketWatcher.Program.05.cs
+++ Snippets/2025-03/MarketWatcher/MarketWatcher.Program.05.cs
@@ -90,7 +90,7 @@ while (true)
         Console.WriteLine();
 
         string message = interpretation != string.Empty
-            ? $"Bot says: RSI signals <b>{rsiStr}{interpretation}</b> for BTC/USDT on Binance!"
+            ? $"Bot says: RSI signals <b>{rsiStr}{interpretation}</b> for BTC/USDT on Binance! Current price is {priceStr} USDT."
             : $"Bot says: RSI does <b>not</b> provide a clear signal for BTC/USDT on Binance! Current price is {priceStr} USDT. RSI value is {rsiStr}.";
 
         await SendTelegramMessageAsync(message);
