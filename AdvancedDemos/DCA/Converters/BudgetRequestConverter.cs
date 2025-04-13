7461cc5abf7366a809d825457e9a1fc8ec30115d
dejfcz <dejfcz@users.noreply.github.com>
2025-04-13 09:54:38 +0100

    json input

---
+++ AdvancedDemos/DCA/Converters/BudgetRequestConverter.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using WhalesSecret.TradeScriptLib.API.TradingV1.Budget;
+
+namespace WhalesSecret.ScriptApiLib.DCA.Converters;
+
+/// <summary>
+/// Custom JSON converter of <see cref="BudgetRequest"/>.
+/// </summary>
+public class BudgetRequestConverter : JsonConverter<BudgetRequest>
+{
+    /// <inheritdoc/>
+    public override BudgetRequest Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        Dictionary<string, object>? budgetData = JsonSerializer.Deserialize<Dictionary<string, object>>(ref reader, options);
+
+        if ((budgetData is null)
+            || !budgetData.TryGetValue("StrategyName", out object? strategyNameObj)
+            || !budgetData.TryGetValue("PrimaryAsset", out object? primaryAssetObj)
+            || !budgetData.TryGetValue("InitialBudget", out object? initialBudgetObj))
+            throw new JsonException("Invalid budget request format.");
+
+        if ((strategyNameObj is not JsonElement strategyNameElement) || (strategyNameElement.ValueKind != JsonValueKind.String))
+            throw new JsonException("Strategy name must be a string.");
+
+        if ((primaryAssetObj is not JsonElement primaryAssetElement) || (primaryAssetElement.ValueKind != JsonValueKind.String))
+            throw new JsonException("Primary asset must be a string.");
+
+        if ((initialBudgetObj is not JsonElement initialBudgetElement) || (initialBudgetElement.ValueKind != JsonValueKind.Object))
+            throw new JsonException("Initial budget must be an object.");
+
+        string strategyName = strategyNameElement.ToString();
+        string primaryAsset = primaryAssetElement.ToString();
+
+        Dictionary<string, decimal>? initialBudget = JsonSerializer.Deserialize<Dictionary<string, decimal>>(initialBudgetObj.ToString()!, options);
+
+        if (initialBudget is null)
+            throw new JsonException("Invalid initial budget format.");
+
+        return new(strategyName: strategyName, primaryAsset: primaryAsset, new BudgetSnapshot(initialBudget));
+    }
+
+    /// <inheritdoc/>
+    public override void Write(Utf8JsonWriter writer, BudgetRequest value, JsonSerializerOptions options)
+        => throw new NotSupportedException();
+}
