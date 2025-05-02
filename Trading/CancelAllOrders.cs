96f1ae76edfb39eb88efa48235418bd29f4550a8
dejfcz <dejfcz@users.noreply.github.com>
2025-05-02 14:53:43 +0100

    Update Trading/CancelAllOrders.cs
    Co-authored-by: MartyIX <203266+MartyIX@users.noreply.github.com>

--- Trading/CancelAllOrders.cs
+++ Trading/CancelAllOrders.cs
@@ -10,7 +10,7 @@ namespace WhalesSecret.ScriptApiLib.Samples.Trading;
 
 /// <summary>
 /// Sample that demonstrates how to cancel all orders.
-/// <para>Private connections are necessary to create orders. Exchange API credentials have to be set.</para>
+/// <para>Private connections are necessary to cancel orders. Exchange API credentials have to be set.</para>
 /// </summary>
 /// <remarks>IMPORTANT: You have to change the secrets in <see cref="Credentials"/> to make the sample work.</remarks>
 public class CancelAllOrders : IScriptApiSample
