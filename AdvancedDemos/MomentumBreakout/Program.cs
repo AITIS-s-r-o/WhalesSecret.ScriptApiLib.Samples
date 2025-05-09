162a8c0b2dae15ce87bee04628b2c94dc1ba8523
dejfcz <dejfcz@users.noreply.github.com>
2025-05-09 10:51:17 +0100

    cs

--- AdvancedDemos/MomentumBreakout/Program.cs
+++ AdvancedDemos/MomentumBreakout/Program.cs
@@ -7,8 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Build.Framework;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.VisualStudio.Threading;
 using Skender.Stock.Indicators;
 using WhalesSecret.ScriptApiLib.Exchanges;
