f31b8bc19dda617ae64db009f45e97bc0652024b
MartyIX <203266+MartyIX@users.noreply.github.com>
2025-05-15 12:59:57 +0200

    WIPWIP

--- Snippets/2025-05/ReconnectingStrategies/ReconnectingStrategies.Program.03.cs
+++ Snippets/2025-05/ReconnectingStrategies/ReconnectingStrategies.Program.03.cs
@@ -5,7 +5,6 @@ using WhalesSecret.TradeScriptLib.API.TradingV1.ConnectionStrategy;
 using WhalesSecret.TradeScriptLib.API.TradingV1.Orders;
 using WhalesSecret.TradeScriptLib.Entities;
 using WhalesSecret.TradeScriptLib.Entities.Orders;
-using WhalesSecret.TradeScriptLib.Exceptions;
 
 // Use the Binance Testnet.
 CreateOptions createOptions = new(connectToBinanceSandbox: true);
@@ -15,9 +14,8 @@ await using ScriptApi scriptApi = await ScriptApi.CreateAsync(createOptions);
 using IApiIdentity apiIdentity = BinanceApiIdentity.CreateHmac(name: "MyApiKey", key: "<KEY>", secret: Convert.FromBase64String("<SECRET>"));
 scriptApi.SetCredentials(apiIdentity);
 
-// Use the FailInstantlyIfNotConnected connection strategy.
-ConnectionOptions connectionOptions = new(FailInstantlyIfNotConnected.Instance, onConnectedAsync: OnConnectedAsync, onDisconnectedAsync: OnDisconnectedAsync);
-
+BlockUntilReconnectedOrTimeout connectionStrategy = new(preRequestTimeout: TimeSpan.FromSeconds(30));
+ConnectionOptions connectionOptions = new(connectionStrategy, onConnectedAsync: OnConnectedAsync, onDisconnectedAsync: OnDisconnectedAsync);
 ITradeApiClient client = await scriptApi.ConnectAsync(ExchangeMarket.BinanceSpot, connectionOptions);
 Print("Connected to Binance sandbox.");
 
@@ -25,36 +23,9 @@ Print("Connected to Binance sandbox.");
 Print("Waiting 60 seconds for you to make sure your machine is offline.");
 await Task.Delay(60_000);
 
-try
-{
-    Print("Attempt to create a market order while disconnected.");
-    await client.CreateMarketOrderAsync(SymbolPair.BTC_EUR, OrderSide.Buy, size: 0.0001m);
-
-    throw new SanityCheckException("Should not be called.");
-}
-catch (NotConnectedException)
-{
-    Print($"{nameof(NotConnectedException)} was thrown as expected!");
-}
-
-try
-{
-    // Similarly, getting open orders will throw the same exception, even
-    // though the method is querying a local storage and does not send an
-    // explicit API request to Binance. The issue is that "not being online
-    // could lead to returning wrong data".
-    Print("Attempt to retrieve all open orders while disconnected.");
-    IReadOnlyList<ILiveOrder> openOrders = await client.GetOpenOrdersAsync();
-
-    throw new SanityCheckException("Should not be called.");
-}
-catch (NotConnectedException)
-{
-    Print($"{nameof(NotConnectedException)} was thrown as expected!");
-}
-
-// Use: sudo netsh interface set interface name="Wi-Fi" admin=ENABLED
-Print("The demonstration is over. You can re-enable your Internet access.");
+// Use: sudo netsh interface set interface name="Wi-Fi" admin=ENABLE
+Print("Waiting 60 seconds for you to make sure your machine is online.");
+await Task.Delay(60_000);
 
 Task OnConnectedAsync(ITradeApiClient tradeApiClient)
 {
